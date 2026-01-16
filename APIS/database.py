e7b4da3c5c8b5a898be58a71ce4d998c1baeff7c
Marcelo <marceloacarlos@gmail.com>
2026-01-16 17:57:20 -0300

    db

--- APIS/database.py
+++ APIS/database.py
@@ -10,7 +10,7 @@ print(f"DATABASE_URL do ambiente: {DATABASE_URL_POSTGRES}")
 
 if not DATABASE_URL_POSTGRES:
     print("⚠️  AVISO: DATABASE_URL não encontrada! Usando fallback localhost")
-    DATABASE_URL_POSTGRES = "postgresql://postgres:admin@localhost:5432/postgres"
+    DATABASE_URL_POSTGRES = "postgresql://admin:KcPUzimXt9Ryvw6dhYsBCviKHoxMFbcm@dpg-d5kihdemcj7s73d7q7m0-a/dbcombustivel"
 
 enginePostgres = create_engine(DATABASE_URL_POSTGRES)
 SessionPostgres = sessionmaker(autocommit=False, autoflush=False, bind=enginePostgres)
